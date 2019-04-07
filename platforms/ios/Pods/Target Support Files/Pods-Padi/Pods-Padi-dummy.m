ef1eb0822d47f485a9510bf193b7eeeb706cd806
Nam Hoai Pham <namhpham@kms-technology.com>
2019-04-08 01:12:35 +0700

    init app

---
+++ platforms/ios/Pods/Target Support Files/Pods-Padi/Pods-Padi-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Padi : NSObject
+@end
+@implementation PodsDummy_Pods_Padi
+@end
---
+++ src/configconstants.ts
@@ -0,0 +1,91 @@
+// (C) Copyright 2015 Martin Dougiamas
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// tslint:disable: variable-name
+export class CoreConfigConstants {
+    static app_id = 'com.moodle.moodlemobile';
+    static appname = 'Moodle Mobile';
+    static desktopappname = 'Moodle Desktop';
+    static versioncode = 3610;
+    static versionname = '3.6.1';
+    static cache_expiration_time = 300000;
+    static default_lang = 'en';
+    static languages: any = {
+        'ar': 'عربي',
+        'bg': 'Български',
+        'ca': 'Català',
+        'cs': 'Čeština',
+        'da': 'Dansk',
+        'de': 'Deutsch',
+        'de-du': 'Deutsch - Du',
+        'el': 'Ελληνικά',
+        'en': 'English',
+        'en-us': 'English - United States',
+        'es': 'Español',
+        'es-mx': 'Español - México',
+        'eu': 'Euskara',
+        'fa': 'فارسی',
+        'fi': 'Suomi',
+        'fr': 'Français',
+        'he': 'עברית',
+        'hi': 'हिंदी',
+        'hr': 'Hrvatski',
+        'hu': 'magyar',
+        'id': 'Indonesian',
+        'it': 'Italiano',
+        'ja': '日本語',
+        'km': 'ខ្មែរ',
+        'kn': 'ಕನ್ನಡ',
+        'ko': '한국어',
+        'lt': 'Lietuvių',
+        'mr': 'मराठी',
+        'nl': 'Nederlands',
+        'no': 'Norsk - bokmål',
+        'pl': 'Polski',
+        'pt': 'Português - Portugal',
+        'pt-br': 'Português - Brasil',
+        'ro': 'Română',
+        'ru': 'Русский',
+        'sr-cr': 'Српски',
+        'sr-lt': 'Srpski',
+        'sv': 'Svenska',
+        'tg': 'Тоҷикӣ',
+        'tr': 'Türkçe',
+        'uk': 'Українська',
+        'zh-cn': '简体中文',
+        'zh-tw': '正體中文'
+    };
+    static wsservice = 'moodle_mobile_app';
+    static wsextservice = 'local_mobile';
+    static demo_sites: any = {
+        student: {
+            url: 'https://school.demo.moodle.net',
+            username: 'student',
+            password: 'moodle'
+        },
+        teacher: {
+            url: 'https://school.demo.moodle.net',
+            username: 'teacher',
+            password: 'moodle'
+        }
+    };
+    static customurlscheme = 'moodlemobile';
+    static siteurl = '';
+    static multisitesdisplay = '';
+    static skipssoconfirmation = false;
+    static forcedefaultlanguage = false;
+    static privacypolicy = 'https://moodle.org/mod/page/view.php?id=8148';
+    static compilationtime = 1554660024599;
+    static lastcommit = '13a9dab037b7eda3b6358c593f3ebbc34c9b4dad';
+}
